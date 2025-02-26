3210a650569010d6faf130aebd2111e17948bec4
Mateo Verea <mateoverea@hotmail.com>
2025-02-25 18:46:11 -0600

    cambios cta

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/navbar"
 import Hero from "@/components/hero"
 import Features from "@/components/features"
-import CTA from "@/components/CTA"
+import CTA from "@/components/cta"
 import Footer from "@/components/footer"
 
 export default function Home() {
